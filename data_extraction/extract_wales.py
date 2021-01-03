9cd6c0a15aa59807b86bd17e688a9d958397b45d
Richard Vaughan <richard@purplemonkeys.co.uk>
2021-01-03 12:44:57 +0000

    Fix Welsh data extractor script.

--- data_extraction/extract_wales.py
+++ data_extraction/extract_wales.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Tests by specimen date')
+rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Tests by specimen date', engine='openpyxl')
 rf.to_csv('../data/uk/wales/tests.csv')
 
-rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Cases by MSOA')
+rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Cases by MSOA', engine='openpyxl')
 rf.to_csv('../data/uk/wales/7_day_averages.csv')
 
-rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Age and sex distribution')
+rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Age and sex distribution', engine='openpyxl')
 rf.to_csv('../data/uk/wales/sex.csv')
 
-rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Deaths by date')
+rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Deaths by date', engine='openpyxl')
 rf.to_csv('../data/uk/wales/deaths.csv')
 
-rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Deaths by LHB')
+rf = pd.read_excel('../uk/wales/Rapid COVID-19 surveillance data.xlsx', sheet_name='Deaths by LHB', engine='openpyxl')
 rf.to_csv('../data/uk/wales/deaths_by_LHB.csv')
